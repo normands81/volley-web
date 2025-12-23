fa34692ef13ccf7e3b5aca79fbe66e0d46e9134f
normands81 <norman.desalvia@gmail.com>
2025-12-23 15:36:36 +0100

    update on supabase config

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -7,4 +7,10 @@ if (!supabaseUrl || !supabaseKey) {
     throw new Error('Missing Supabase URL or Key in environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true
+    }
+});
